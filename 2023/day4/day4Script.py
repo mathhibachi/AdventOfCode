d179b4b05bc2277b3f292ca1ff3d0169d0f000af
mathhibachi <mathhibachi@gmail.com>
2023-12-04 01:40:35 -0600

    Update day4Script.py

--- 2023/day4/day4Script.py
+++ 2023/day4/day4Script.py
@@ -11,13 +11,19 @@ data = get_data(day=4, year=2023).splitlines()
 
 points = 0
 
-[cardCounts[x] = 1 for x in data]
+cardCounts = {}
 
-with open(fin) as f:
-  for line in f.readlines():
+for line in data:
+#with open(fin) as f:
+#  for line in f.readlines():
+      card = line.split(': ')[0].split(' ')[-1]
+      print(int(card))
+      cardCounts[int(card)] = 1
 
-# for line in data:
-    card = line.split(': ')[0].split(' ')[1]
+for line in data:
+#with open(fin) as f:
+#  for line in f.readlines():
+    card = line.split(': ')[0].split(' ')[-1]
     winNums = line.split(': ')[1].split(' | ')[0].split(' ')
     winNums = [x.strip() for x in winNums if x != '']
     cardNums = line.split(': ')[1].split(' | ')[1].split(' ')
@@ -27,9 +33,18 @@ with open(fin) as f:
         if cardNum in winNums:
             count += 1
     
+    for i in range(count):
+        if i<len(cardCounts.keys())-1:
+            cardCounts[int(card)+(i+1)] += 1 * cardCounts[int(card)]
     
     if count>0:
         print(card,count)
-        points += 2**(count-1)
+        points += cardCounts[int(card)] * 2**(count-1)
         
 print(points)
+print(cardCounts)
+
+cardSums = 0
+for k,v in cardCounts.items():
+    cardSums+=v
+print(cardSums)
