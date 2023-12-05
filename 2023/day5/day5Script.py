4ef6703b1f181512a280f4c3e46843994dc4fe5c
mathhibachi <mathhibachi@gmail.com>
2023-12-05 01:42:43 -0600

    Create day5Script.py

---
+++ 2023/day5/day5Script.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+from aocd.models import Puzzle
+
+puz = Puzzle(year=2023,day=5)
+exData = puz.example_data.splitlines()
+inData = puz.input_data.splitlines()
+
+exData = [x for x in exData if len(x)>0]
+inData = [x for x in inData if len(x)>0]
+
+class plantingMap:
+    def __init__(self,name):
+        self.name = name
+        self.srcStart = []
+        self.srcRng = []
+        self.dstStart = []
+        
+        self.inputs = []
+        self.outputs = []
+        
+    def addLine(self,line):
+        line = [int(x) for x in line]
+        self.srcStart.append(line[1])
+        self.srcRng.append(line[2])
+        self.dstStart.append(line[0])
+        
+        for x in range(line[1],line[1]+line[2]):
+            self.inputs.append(x)
+        for x in range(line[0],line[0]+line[2]):
+            self.outputs.append(x)
+    
+    def getOutput(self,inValue):
+        if inValue in self.inputs:
+            return self.outputs[self.inputs.index(inValue)]
+        else:
+            return inValue
+                            
+        
+section = ''
+allMaps = []
+maps = {}
+for line in inData:
+    line = line.split()
+    
+    if line[0] == 'seeds:':
+        seeds = line[1:]
+    elif line[0] == 'seed-to-soil':
+        section = 'seed2soil'
+        maps[section] = plantingMap(section)
+        print(section)
+    elif line[0] == 'soil-to-fertilizer':
+        section = 'soil2fert'
+        maps[section] = plantingMap(section)
+        print(section)
+    elif line[0] == 'fertilizer-to-water':
+        section = 'fert2water'
+        maps[section] = plantingMap(section)
+        print(section)
+    elif line[0] == 'water-to-light':
+        section = 'water2light'
+        maps[section] = plantingMap(section)
+        print(section)
+    elif line[0] == 'light-to-temperature':
+        section = 'light2temp'
+        maps[section] = plantingMap(section)
+        print(section)
+    elif line[0] == 'temperature-to-humidity':
+        section = 'temp2humidity'
+        maps[section] = plantingMap(section)
+        print(section)
+    elif line[0] == 'humidity-to-location':
+        section = 'humidity2loc'
+        maps[section] = plantingMap(section)
+        print(section)
+    else:
+        print(line)
+        maps[section].addLine(line)
+
+for x in maps:
+    allMaps.append(maps[x])
+
+routes = []
+locs = []
+for seed in seeds:
+    x = int(seed)
+    route = [x]
+    for plantMap in allMaps:
+        x = plantMap.getOutput(x)
+        route.append(x)
+        
+    print(seed,x,route)
+    locs.append(x)
+    #print(y)
+print(min(locs))
+# print(maps['seed2soil'].inputs)
+# print(maps['seed2soil'].outputs)
