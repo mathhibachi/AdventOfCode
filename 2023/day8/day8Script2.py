d584d95dcb3c2a42fbda71b82f07721e9a52b105
mathhibachi <mathhibachi@gmail.com>
2023-12-09 00:40:33 -0600

    Update day8Script2.py

--- 2023/day8/day8Script2.py
+++ 2023/day8/day8Script2.py
@@ -1,93 +1,70 @@
 # -*- coding: utf-8 -*-
-
 from aocd.models import Puzzle
 import threading
+import math
 
-# Started down the path of threading but ended up abandoning
+# Started down the path of threading but ended up abandoning (until next night when I got this working :)
 
-def runStuff():
-    
-    puz = Puzzle(year=2023,day=8)
-    # Example input data
-    exData = puz.example_data.splitlines()
-    # Full input data
-    inData = puz.input_data.splitlines()
-    
-    exData2 = []
-    with open('day8Input3.txt') as f:
-      for line in f.readlines():
-          exData2.append(line.strip())
-    
-    data = inData
-    directions = data[0]
-    
-    turnData = data[2:]
-    
-    turns = {}
-    inTurns = []
-    outTurns = {'L':[],'R':[]}
-    for line in turnData:
-        inTurns.append(line.split(' ')[0])
-        outTurns['L'].append(line.split('(')[1].split(',')[0])
-        turns[line.split(' ')[0]] = {'L':line.split('(')[1].split(',')[0],
-                                     'R':line.split(', ')[1].split(')')[0]}
-    
-    inputs = [x for x in inTurns if x[-1]=='A']
+puz = Puzzle(year=2023,day=8)
+# Example input data
+exData = puz.example_data.splitlines()
+# Full input data
+inData = puz.input_data.splitlines()
+
+# If reading in from local file
+# exData2 = []
+# with open('day9Input3.txt') as f:
+#   for line in f.readlines():
+#       exData2.append(line.strip())
+
+# Set data set to use for testing
+data = inData
+
+# LRRLL...
+directions = data[0]
+
+# AAA = (BBB, CCC)
+turnData = data[2:]
+
+def doStuff(counts,idx,turns,inp,directions):
     count = 0
-    
-    i = count % len(directions)
-    
-    t = [None]*len(inputs)
-    results = [None]*len(inputs)
-    for i in range(len(threads)):
-        t[i] = threading.Thread(target=findPaths, 
-                          args=(inputs[0],directions[i],turns,results,i))
-        
-        t[i].start()
-        
-    for i in range(len(threads)):
-        t[i].join()
-    
-    print(results)
+    while count < 100000:
+        i = count % len(directions)
+        out = turns[inp][directions[i]]
+        if out[-1] == 'Z':
+            counts.append(count+1)
+            break
+        count += 1
+        inp = out
+    counts[idx] = count+1
+    #print(counts)
 
-def findPaths(inp,direction,turns,result,idx):
-    out = turns[inp][direction]
-    if out[-1] == 'Z':
-        result[idx] = True
-    else:
-        result[idx] = False
-    inp = out
-    return out        
-     
-    # temp = [x for x in outs if x[-1]=='Z']
-    #         if len(temp) == len(outs):
-    #             print('Made it in '+str(count+1)+' turns')
-    #             break
-            # count += 1
-            # inputs = outs
+# turns['AAA'] = {'L':'BBB', 'R':'CCC'}
+turns = {}
+# inTurns = ['AAA',...]
+inTurns = []
+for line in turnData:
+    inTurns.append(line.split(' ')[0])
+    turns[line.split(' ')[0]] = {'L':line.split('(')[1].split(',')[0],
+                                 'R':line.split(', ')[1].split(')')[0]}
 
-if __name__=="__main__":
-    runStuff()
+inputs = [x for x in inTurns if x[-1]=='A']
+counts = []
+threads = []
+# Gather up how many turns it takes to go from initial value to end (for each input)
+for i,inp in enumerate(inputs):
+    count = 0
+    threads.append(threading.Thread(target=doStuff,\
+                          args=(counts,i,turns,inp,directions)))
+    threads[i].start()
     
+for i,inp in enumerate(inputs):
+    threads[i].join()
     
-    
-#print(turns)
-#inp = inTurns[0]
-#inp = 'AAA'
-# count = 0
-# while count<1000000:
-#     i = count % len(directions)
-#     #print(i)
-#     if inp in inTurns:
-#         out = turns[inp][directions[i]]
-#     else:
-#         print('Not a valid input')
-#         break
-#     #print(inp,out)
-#     if out=='ZZZ':
-#         print('Made it in '+str(count+1)+' turns')
-#         break
-#     count += 1
-#     inp = out
-    
-# re.find   
+print(counts)
+# LCM not available in python 3.8, so used GCD
+temp = 1
+for i in counts:
+    temp = temp*i // math.gcd(temp,i)
+
+print(temp)
